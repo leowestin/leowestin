43a5f67708a820c806590f3e086614220ecaade7
leowestin <60442109+leowestin@users.noreply.github.com>
2020-02-15 12:24:13 +0100

    Add files via upload

--- numpyarray.py
+++ numpyarray.py
@@ -3,11 +3,13 @@ import string
 
 letters_of_password = int(input("How many letters do you want in your password?: "))
 numbers_of_password = int(input("How many numbers do you want in your password?: "))
+specialdigits_of_password = int(input("How many special digits do you want in your password?: "))
 
 number_list = ''.join(random.choice(string.digits) for i in range(numbers_of_password))
 letter_list = ''.join(random.choice(string.ascii_letters) for i in range(letters_of_password))
+special_list = ''.join(random.choice(string.punctuation) for i in range(specialdigits_of_password))
 
-syntax_list = letter_list + number_list + '!#@'
+syntax_list = letter_list + number_list + special_list
 if len(syntax_list) < 6:
     print("Your password needs to be more than 6 characters long")
     exit()
