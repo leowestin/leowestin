373324829dbf2ccf4ceedab405ae719803a4f4a4
leowestin <60442109+leowestin@users.noreply.github.com>
2020-02-22 17:22:25 +0100

    Add files via upload

---
+++ hallo.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+letters_of_password = int(input("How many letters do you want in your password?: "))
+numbers_of_password = int(input("How many numbers do you want in your password?: "))
+specialdigits_of_password = int(input("How many special digits do you want in your password?: "))
+
+number_list = ''.join(random.choice(string.digits) for i in range(numbers_of_password))
+letter_list = ''.join(random.choice(string.ascii_letters) for i in range(letters_of_password))
+special_list = ''.join(random.choice(string.punctuation) for i in range(specialdigits_of_password))
+
+syntax_list = number_list + letter_list + special_list
+
+syntax_list_recursive = 1
+
+def numbers_recursive():
+    
+    if len(syntax_list) >= 6:
+        return syntax_list
+        
+    else:
+        print("Your password needs to be atleast 6 characters long")
+        syntax_list = syntax_list + syntax_list_recursive
+        return numbers_recursive
+
+
+    
+
+result = random.sample(syntax_list, len(syntax_list))
+password = ''.join(result)
+
+print("Your password is {0}".format(password))
